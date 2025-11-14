07b8aa41a7da80f490940403ca316225510ecfc3
Axton Excell <axton.s.excell@gmail.com>
2025-11-14 14:29:34 -0800

    preImages

--- Code camp game.py
+++ Code camp game.py
@@ -2,10 +2,13 @@ import pygame
 pygame.init
 
 # World Vars
+pygame.font.init()
 run = True
 screenD = [1000, 800]
 screen = pygame.display.set_mode((screenD))
 friction = 0.08
+fonts = pygame.font.Font(None, 50)
+
 
 # Squares
 squares = [
@@ -24,8 +27,7 @@ squares = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
 def displaySquares(squares):
@@ -43,14 +45,14 @@ def displaySquares(squares):
 
 # Player Vars
 class player(pygame.sprite.Sprite):
-    def __init__(self,location,playerNumber):
+    def __init__(self,location,playerNumber,images):
         pygame.sprite.Sprite.__init__(self)
         self.image = []
         self.playerMomentum = [0, 0]
         self.playerLocation = location
         self.playerNumber = playerNumber
-        self.image.append(pygame.image.load('greeny.png').convert_alpha())
-        self.image.append(pygame.image.load('greeny.png').convert_alpha())
+        self.image.append(pygame.image.load(images[0]).convert_alpha())
+        self.image.append(pygame.image.load(images[1]).convert_alpha())
         self.rect = self.image[0].get_rect()
         self.rect.left,self.rect.top = location
         self.anamationFrame = 0
@@ -111,18 +113,30 @@ class player(pygame.sprite.Sprite):
         cerentFrame = 0
         if self.anamationFrame > 30:
             cerentFrame = 1
-        
-        screen.blit(self.image[cerentFrame],self.rect)
-        
-        
+        screen.blit(self.image[cerentFrame],self.rect)    
+    def score(self):
+        points = 0
+        for row in squares:
+            for square in row:
+                if square == self.playerNumber:
+                    points += 1
+        return points
+                
 
 
 
 clock = pygame.time.Clock()
 playerLocation = [500, 500]
 # Player Updates
-player1 = player([500, 500], 1)
-player2 = player([500, 500], 2)
+player1 = player([500, 500], 1, ['images/greeny.png', 'images/greeny.png'])
+player2 = player([500, 500], 2, ['images/greeny.png', 'images/greeny.png'])
+
+def paint(player):
+    x = int((player.playerLocation[0]+25)/50)
+    y = int((player.playerLocation[1]+25)/50)
+    squares[y][x] = player.playerNumber
+
+
 while run:
     
     # Frame Rate Stuff
@@ -138,9 +152,17 @@ while run:
     player1.playerMovment()
     player2.playerMovment()
 
+    paint(player1)
+    paint(player2)
+    player1score = player1.score()
+    player2score = player2.score()
+    text1 = fonts.render(str(player1score), True, (255, 255, 255), None)
+    text2 = fonts.render(str(player2score), True, (255, 255, 255), None)
     # Display 
     screen.fill((100, 100, 100))
     displaySquares(squares)
     player1.Draw(screen)
     player2.Draw(screen)
+    screen.blit(text1, (20, 10))
+    screen.blit(text2, (screenD[0] - 100 ,10))
     pygame.display.flip()
